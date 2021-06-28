69fc2c62c578d69abda53c56a9701386c502012b
Anna <anna.russkova@gmail.com>
2021-06-28 14:46:29 +0300

    OpenGraph & Favicon

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -15,7 +15,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'anna.russkova@gmail.com';                 // Наш логин
-$mail->Password = '!!!пароль от почты';                           // Наш пароль от ящика
+$mail->Password = 'password';                          // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
